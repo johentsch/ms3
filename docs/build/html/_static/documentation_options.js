fefa5761fdb300ae62968adf59f60f82568fc6b4
Johannes Hentschel <johannes.hentschel@epfl.ch>
2020-11-02 22:40:29 +0100

    small update to docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0-docs.post0.dev6-g8096cb1.dirty',
+    VERSION: '0.4.1.post0.dev4+g619dcae',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
