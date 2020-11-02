4627f1386f51720ca324ae460c23cacbdd8e8bff
laserjeyes <johannes.hentschel@epfl.ch>
2020-11-02 11:21:58 +0100

    Implementing file conversion

--- src/ms3/cli.py
+++ src/ms3/cli.py
@@ -17,7 +17,8 @@ Note: This skeleton file can be safely removed if not needed!
 
 import argparse, os
 
-from ms3 import Parse, __version__
+from ms3 import Parse
+from ms3.convert import convert_folder
 from ms3.utils import resolve_dir
 
 __author__ = "johentsch"
@@ -83,18 +84,18 @@ def check_dir(d):
 
 
 def convert(args):
-    dir, target = os.path.realpath(args.dir), os.path.realpath(args.target)
-    assert target[:len(
-        dir)] != dir, "TARGET_DIR cannot be identical with nor a subfolder of DIR.\nDIR:        " + dir + '\nTARGET_DIR: ' + target
+    dir, target = resolve_dir(args.dir), resolve_dir(args.target)
+    #assert target[:len(
+    #    dir)] != dir, "TARGET_DIR cannot be identical with nor a subfolder of DIR.\nDIR:        " + dir + '\nTARGET_DIR: ' + target
     convert_folder(dir, target,
                    extensions=args.extensions,
                    target_extension=args.format,
                    regex=args.regex,
                    suffix=args.suffix,
-                   recursive=not args.non_recursive,
+                   recursive=args.nonrecursive,
                    MS=args.musescore,
                    overwrite=args.overwrite,
-                   parallel=args.parallel)
+                   parallel=args.nonparallel)
 
 def run():
     """Entry point for console_scripts
@@ -103,12 +104,11 @@ def run():
         prog='ms3',
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description='''\
-------------------------------------
-| Format Mozart data to your needs |
-------------------------------------
+--------------------------
+| Welcome to ms3 Parsing |
+--------------------------
 
-This script needs to remain at the top level of (your local copy of) the repo  mozart_piano_sonatas.
-Run with parameter -t to see all file names.
+The library offers you the following commands. Add the flag -h to one of them to learn about its parameters. 
 ''')
 
 
@@ -140,7 +140,7 @@ This setting has no effect on absolute folder paths.""")
     extract_parser.add_argument('-t', '--test', action='store_true', help="No data is written to disk.")
     extract_parser.add_argument('-p', '--positioning', action='store_true', help="When extracting labels, include their spacial positionings in order to restore them when re-inserting.")
     extract_parser.add_argument('-r', '--raw', action='store_false', help="When extracting labels, leave chord symbols encoded instead of turning them into strings.")
-    extract_parser.add_argument('--nonrecursive', action='store_false', help="Don't scan folders recursively, i.e. parse only files in MSCX_DIR.")
+    extract_parser.add_argument('-n', '--nonrecursive', action='store_false', help="Don't scan folders recursively, i.e. parse only files in MSCX_DIR.")
     extract_parser.set_defaults(func=extract)
 
     check_parser = subparsers.add_parser('check', help="Check DCML harmony labels.")
@@ -150,15 +150,10 @@ This setting has no effect on absolute folder paths.""")
 
     convert_parser = subparsers.add_parser('convert',
                                            help="Use your local install of MuseScore to convert MuseScore files.")
-    convert_parser = argparse.ArgumentParser(description="""Tries to convert all files in DIR that have given extension(s) using a MuseScore executable.
-
-    By default, this is done recursively on subfolders and the folder structure is mirrored in TARGET_DIR,
-    assuming that the standard use case is conversion from an older to a newer MuseScore version.""",
-                                             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     convert_parser.add_argument('dir', metavar='DIR', type=check_dir,
                                 help='path to folder with files to convert; can be relative to the folder where the script is located.')
-    convert_parser.add_argument('target', metavar='TARGET_DIR', nargs='?', type=check_dir, default=os.getcwd(),
-                                help='path to folder for converted files.')
+    convert_parser.add_argument('target', metavar='TARGET_DIR', nargs='?', type=check_and_create, default=os.getcwd(),
+                                help='Path to folder for converted files. Defaults to current working directory.')
     convert_parser.add_argument('-e', '--extensions', nargs='+', default=['mscx', 'mscz'],
                                 help="List, separated by spaces, the file extensions that you want to convert. Defaults to mscx mscz")
     convert_parser.add_argument('-f', '--format', default='mscx',
@@ -167,12 +162,12 @@ This setting has no effect on absolute folder paths.""")
     To use standard paths on commercial systems, try -m win, or -m mac.""")
     convert_parser.add_argument('-r', '--regex', default=r'.*',
                                 help="Convert only files containing this regular expression (or a simple search string).")
-    extract_parser.add_argument('--nonrecursive', action='store_false',
+    convert_parser.add_argument('-n', '--nonrecursive', action='store_false',
                                 help="Don't scan folders recursively, i.e. parse only files in DIR.")
     convert_parser.add_argument('-o', '--overwrite', action='store_true',
                                 help="Set true if existing files are to be overwritten.")
-    convert_parser.add_argument('-p', '--parallel', action='store_true',
-                                help="Use all available CPU cores in parallel to speed up batch jobs.")
+    convert_parser.add_argument('-p', '--nonparallel', action='store_false',
+                                help="Do not use all available CPU cores in parallel to speed up batch jobs.")
     convert_parser.add_argument('-s', '--suffix', metavar='SUFFIX', help='Add this suffix to the filename of every new file.')
     convert_parser.set_defaults(func=convert)
 
