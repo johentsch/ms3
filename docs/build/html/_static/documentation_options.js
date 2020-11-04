22d373a3a217da0cbf83d53a9656a85a1f857b7c
laserjeyes <johannes.hentschel@epfl.ch>
2020-11-04 17:13:23 +0100

    updated docs to 0.4.2

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1.post0.dev4+g619dcae',
+    VERSION: '0.4.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
