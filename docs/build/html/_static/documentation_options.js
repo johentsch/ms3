1aacb2535644bd51ea40de1c1234540ee6babea1
johentsch <johannes.hentschel@epfl.ch>
2021-10-05 13:05:48 +0200

    updated docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.8.post0.dev12+gf2419e0.dirty',
+    VERSION: '0.4.9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
