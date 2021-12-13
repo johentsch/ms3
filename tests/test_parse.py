2919295477d5230c96fd8ea2e4469aa00b6c4550
Johannes Hentschel <johannes.hentschel@epfl.ch>
2021-12-13 12:05:00 +0100

    Added unittest for Parse

---
+++ tests/test_parse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+from ms3 import Parse, resolve_dir
+
+def test_folder_parse():
+    p = Parse(".")
+    assert isinstance(p, Parse), "Failed to parse folder."
+
+def test_paths_parse():
+    here = Path(resolve_dir('.'))
+    exts = [".mscx", ".tsv"]
+    paths = [str(p) for p in here.glob("**/*") if p.suffix in exts]
+    print(paths)
+    p = Parse(paths=paths, logger_cfg=dict(level='d'))
+    assert isinstance(p, Parse), "Failed to parse list of paths."
