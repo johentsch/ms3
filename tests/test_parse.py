d645a6a244b43e0d32105d4df4bdd262ad983d49
johentsch <johannes.hentschel@epfl.ch>
2021-12-14 23:32:03 +0100

    Preparations for test_parse

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from pathlib import Path
-
+import pytest
 from ms3 import Parse, resolve_dir
 
 def test_folder_parse():
@@ -14,3 +14,23 @@ def test_paths_parse():
     paths = [str(p) for p in here.glob("**/*") if p.suffix in exts]
     p = Parse(paths=paths, logger_cfg=dict(level='d'))
     assert isinstance(p, Parse), "Failed to parse list of paths."
+
+
+@pytest.fixture
+def parsed_mscx():
+    p = Parse('MS3', file_re='mscx$', logger_cfg=dict(level='d'))
+    p.parse()
+    return p
+
+
+class TestParse:
+
+    test_folder = os.path.dirname(os.path.realpath(__file__))
+
+    def test_extract(self, parsed_mscx):
+        target = resolve_dir('test_results')
+        path_dict = parsed_mscx.store_lists(measures_folder=target, measures_suffix="_measures",
+                                notes_folder=target, notes_suffix='_notes',
+                                labels_folder=target, labels_suffix='_labels')
+        for path, what in path_dict.items():
+            original_path = os.path.join(test_folder, what, )
