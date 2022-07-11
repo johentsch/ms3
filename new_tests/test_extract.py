dd42d6182ca9de497a9befe7e955a4a9587ebbd4
johentsch <johannes.hentschel@epfl.ch>
2022-07-11 15:05:28 +0200

    first steps toward the new unittest interface

--- new_tests/test_extract.py
+++ new_tests/test_extract.py
@@ -1,4 +1,5 @@
 import pytest
+import ms3
 
 TEST_NAMES = [
  'multiple-all_paths',
@@ -36,49 +37,162 @@ def test_collect_cfg_names(parse_obj, collect_cfg_name):
 
 ################################## Actual tests ############################################
 
-@pytest.fixture()
-def expected_keys(request):
-    expected = [
-        {'mixed_files': {'.mscx': 8,                        # multiple-all_paths
-                         '.mscz': 1,
-                         '.musicxml': 1,
-                         '.mxl': 1,
-                         '.tsv': 1,
-                         '.xml': 1},
-         'ravel_piano': {'.mscx': 5, '.tsv': 14},
-         'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
-         'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
-        {'mixed_files': {'.mscx': 8, '.mscz': 1, '.tsv': 1}, # multiple-directory
-         'ravel_piano': {'.mscx': 5, '.tsv': 14},
-         'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
-         'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
-        {'mixed_files': {'.mscx': 8,                        # multiple-directory+paths
-                         '.mscz': 1,
-                         '.musicxml': 1,
-                         '.mxl': 1,
-                         '.tsv': 1,
-                         '.xml': 1},
-         'ravel_piano': {'.mscx': 5, '.tsv': 14},
-         'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
-         'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
-        {'mixed_files': {'.mscx': 8},                       # multiple-mscx_paths
-         'ravel_piano': {'.mscx': 5},
-         'sweelinck_keyboard': {'.mscx': 1},
-         'wagner_overtures': {'.mscx': 2}},
-        {'mixed_files': {'.tsv': 1},                        # multiple-tsv_paths
-         'ravel_piano': {'.tsv': 14},
-         'sweelinck_keyboard': {'.tsv': 4},
-         'wagner_overtures': {'.tsv': 7}},
-        {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-all_paths
-        {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory
-        {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory+paths
-        {'pleyel_quartets': {'.mscx': 6}},                  # single-mscx_paths
-        {'pleyel_quartets': {'.tsv': 13}},                  # single-tsv_paths
-    ]
-    name2expected = dict(zip(TEST_NAMES, expected))
-    name = node_name2cfg_name(request.node.name)
-    return name2expected[name]
+class TestParseObject():
 
-def test_parse_keys(parse_obj, expected_keys):
-    assert parse_obj.count_extensions(per_key=True) == expected_keys
+    @pytest.fixture()
+    def expected_keys(self, request):
+        expected = [
+            {'mixed_files': {'.mscx': 8,                        # multiple-all_paths
+                             '.mscz': 1,
+                             '.musicxml': 1,
+                             '.mxl': 1,
+                             '.tsv': 1,
+                             '.xml': 1},
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8, '.mscz': 1, '.tsv': 1}, # multiple-directory
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8,                        # multiple-directory+paths
+                             '.mscz': 1,
+                             '.musicxml': 1,
+                             '.mxl': 1,
+                             '.tsv': 1,
+                             '.xml': 1},
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8},                       # multiple-mscx_paths
+             'ravel_piano': {'.mscx': 5},
+             'sweelinck_keyboard': {'.mscx': 1},
+             'wagner_overtures': {'.mscx': 2}},
+            {'mixed_files': {'.tsv': 1},                        # multiple-tsv_paths
+             'ravel_piano': {'.tsv': 14},
+             'sweelinck_keyboard': {'.tsv': 4},
+             'wagner_overtures': {'.tsv': 7}},
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-all_paths
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory+paths
+            {'pleyel_quartets': {'.mscx': 6}},                  # single-mscx_paths
+            {'pleyel_quartets': {'.tsv': 13}},                  # single-tsv_paths
+        ]
+        name2expected = dict(zip(TEST_NAMES, expected))
+        name = node_name2cfg_name(request.node.name)
+        return name2expected[name]
+
+    def test_parse_keys(self, parse_obj, expected_keys):
+        assert parse_obj.count_extensions(per_key=True) == expected_keys
+
+
+@pytest.mark.usefixtures("parse_objects")
+class TestInstantiation():
+
+    @pytest.fixture()
+    def expected_keys(self, request):
+        expected = [
+            {'mixed_files': {'.mscx': 8,                        # multiple-all_paths
+                             '.mscz': 1,
+                             '.musicxml': 1,
+                             '.mxl': 1,
+                             '.tsv': 1,
+                             '.xml': 1},
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8, '.mscz': 1, '.tsv': 1}, # multiple-directory
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8,                        # multiple-directory+paths
+                             '.mscz': 1,
+                             '.musicxml': 1,
+                             '.mxl': 1,
+                             '.tsv': 1,
+                             '.xml': 1},
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8},                       # multiple-mscx_paths
+             'ravel_piano': {'.mscx': 5},
+             'sweelinck_keyboard': {'.mscx': 1},
+             'wagner_overtures': {'.mscx': 2}},
+            {'mixed_files': {'.tsv': 1},                        # multiple-tsv_paths
+             'ravel_piano': {'.tsv': 14},
+             'sweelinck_keyboard': {'.tsv': 4},
+             'wagner_overtures': {'.tsv': 7}},
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-all_paths
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory+paths
+            {'pleyel_quartets': {'.mscx': 6}},                  # single-mscx_paths
+            {'pleyel_quartets': {'.tsv': 13}},                  # single-tsv_paths
+        ]
+        name2expected = dict(zip(TEST_NAMES, expected))
+        name = node_name2cfg_name(request.node.name)
+        return name2expected[name]
+
+    def test(self, expected_keys):
+        assert isinstance(parsed_parse_obj, ms3.Parse)
+
+
+@pytest.mark.usefixtures("parse_objects")
+class TestKeys():
+
+    @pytest.fixture()
+    def expected_keys(self, request):
+        expected = [
+            {'mixed_files': {'.mscx': 8,                        # multiple-all_paths
+                             '.mscz': 1,
+                             '.musicxml': 1,
+                             '.mxl': 1,
+                             '.tsv': 1,
+                             '.xml': 1},
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8, '.mscz': 1, '.tsv': 1}, # multiple-directory
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8,                        # multiple-directory+paths
+                             '.mscz': 1,
+                             '.musicxml': 1,
+                             '.mxl': 1,
+                             '.tsv': 1,
+                             '.xml': 1},
+             'ravel_piano': {'.mscx': 5, '.tsv': 14},
+             'sweelinck_keyboard': {'.mscx': 1, '.tsv': 4},
+             'wagner_overtures': {'.mscx': 2, '.tsv': 7}},
+            {'mixed_files': {'.mscx': 8},                       # multiple-mscx_paths
+             'ravel_piano': {'.mscx': 5},
+             'sweelinck_keyboard': {'.mscx': 1},
+             'wagner_overtures': {'.mscx': 2}},
+            {'mixed_files': {'.tsv': 1},                        # multiple-tsv_paths
+             'ravel_piano': {'.tsv': 14},
+             'sweelinck_keyboard': {'.tsv': 4},
+             'wagner_overtures': {'.tsv': 7}},
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-all_paths
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory
+            {'pleyel_quartets': {'.mscx': 6, '.tsv': 13}},      # single-directory+paths
+            {'pleyel_quartets': {'.mscx': 6}},                  # single-mscx_paths
+            {'pleyel_quartets': {'.tsv': 13}},                  # single-tsv_paths
+        ]
+        name2expected = dict(zip(TEST_NAMES, expected))
+        name = node_name2cfg_name(request.node.name)
+        return name2expected[name]
+
+    def test(self, expected_keys):
+        assert self.parse_obj.count_extensions(per_key=True) == expected_keys
+
+
+def test_parsed(parsed_parse_obj):
+
+
+def test_concatenated_facets(parsed_parse_obj):
+    notes = parsed_parse_obj.notes()
+    assert len(notes) > 0
+
+# add_dir (different keys)
+# file_re
 
