ae3a86f6c4ca8e52d4db5184aa75ef50272e679b
johentsch <johannes.hentschel@epfl.ch>
2022-10-04 16:24:44 +0200

    MSCX.measures() comes without 'volta' column if no alternative endings are present

--- src/ms3/bs4_parser.py
+++ src/ms3/bs4_parser.py
@@ -484,6 +484,8 @@ Use 'ms3 convert' command or pass parameter 'ms' to Score to temporally convert.
                 .shift(fill_value=0)\
                 .reindex(measures.index)
             measures.insert(2, "quarterbeats", quarterbeats_col * 4)
+        else:
+            measures.drop(columns='volta', inplace=True)
         return measures.copy()
 
     @property
