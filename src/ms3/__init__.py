f5eebdc7037cd2116b8446ba2c8eb418ea9a0691
johentsch <johannes.hentschel@epfl.ch>
2022-10-22 01:23:45 +0200

    makes Piece importable from ms3

--- src/ms3/__init__.py
+++ src/ms3/__init__.py
@@ -20,6 +20,7 @@ finally:
 from .score import Score
 from .annotations import Annotations
 from .parse import Parse
+from .piece import Piece
 from .corpus import Corpus
 from .utils import *
 from .transformations import *
