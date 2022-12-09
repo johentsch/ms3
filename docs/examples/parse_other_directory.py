0a52eb593a7352cb2091d26d1479fe27706cc3b8
johentsch <johannes.hentschel@epfl.ch>
2022-12-10 00:32:58 +0100

    removes parameter 'key' from manual's examples

--- docs/examples/parse_other_directory.py
+++ docs/examples/parse_other_directory.py
@@ -1,6 +1,6 @@
 from ms3 import Parse
 import os
-p = Parse('..', file_re="mscx$", key='ms3')
+p = Parse('..', file_re="mscx$")
 mozart_path = os.path.join('~', 'mozart_piano_sonatas')
-p.add_corpus(mozart_path, file_re="mscx$", corpus_name='other')
+p.add_corpus(mozart_path, file_re="mscx$")
 print(p)
