1fa0042e1823c83ac8ff6d2879ca534ea3a93ae1
johentsch <johannes.hentschel@epfl.ch>
2023-06-25 21:22:29 +0200

    schemas unequivocally named according to a truncated hash of their column names; columns parsed via ast.literal_eval are interpreted as type='array'; make_frictionless_schema_descriptor accepts primary_key argument;

--- src/ms3/utils/frictionless.py
+++ src/ms3/utils/frictionless.py
@@ -1,9 +1,12 @@
+import hashlib
 import json
 import os
 import re
+from ast import literal_eval
+from base64 import urlsafe_b64encode
 from functools import cache
-from pprint import pformat
-from typing import Sequence
+from pprint import pprint
+from typing import Optional, Tuple, Iterable
 
 import frictionless as fl
 import pandas as pd
@@ -37,8 +40,6 @@ def column_name2frictionless_field(column_name) -> dict:
         field['title'] = title
     if description:
         field['description'] = description
-    if string_converter is not None:
-        pass
     if string_converter:
         if string_converter == safe_frac:
             field['type'] = 'string'
@@ -51,6 +52,8 @@ def column_name2frictionless_field(column_name) -> dict:
             constraints["pattern"] = INT_ARRAY_REGEX
         elif string_converter == int2bool:
             field['type'] = 'boolean'
+        elif string_converter == literal_eval:
+            field['type'] = 'array'
         else:
             NotImplementedError(f"Unfamiliar with string converter {string_converter}")
     elif pandas_dtype:
@@ -61,20 +64,29 @@ def column_name2frictionless_field(column_name) -> dict:
         elif pandas_dtype in (str, 'string'):
             field['type'] = 'string'
         else:
-            NotImplementedError(f"Don't know how to handle pandas dtype {pandas_dtype}")
+            raise NotImplementedError(f"Don't know how to handle pandas dtype {pandas_dtype}")
     else:
-        NotImplementedError(f"Don't know how to handle column {column_name}")
+        raise NotImplementedError(f"Don't know how to handle column {column_name}")
     if len(constraints) > 0:
         field['constraints'] = constraints
     return field
 
 
-def make_frictionless_descriptor(column_names: Sequence[str]) -> dict:
+def make_frictionless_schema_descriptor(column_names: Tuple[str, ...],
+                                        primary_key: Optional[Tuple[str, ...]] = None,
+                                        **custom_data
+                                        ) -> dict:
     fields = []
     for column_name in column_names:
         field = column_name2frictionless_field(column_name)
+        if not "type" in field:
+            raise ValueError(f"column_name2frictionless_field({column_name!r}) = {field} (missing 'type'!)")
         fields.append(field)
     descriptor = dict(fields=fields)
+    if primary_key:
+        descriptor['primaryKey'] = primary_key
+    if len(custom_data) > 0:
+        descriptor.update(custom_data)
     return descriptor
 
 FRICTIONLESS_REGEX = r"^([-a-z0-9._/])+$"
@@ -87,14 +99,26 @@ def make_resource_name(name: str, replace_char="_") -> str:
         name = re.sub(FRICTIONLESS_INVERSE, replace_char, name)
     return name
 
-def assemble_resource_descriptor(name, path, schema):
-    return {
+def assemble_resource_descriptor(name: str,
+                                 path: str,
+                                 schema: str,
+                                 innerpath: Optional[str] = None,
+                                 ):
+    is_zipped = path.endswith(".zip")
+    if is_zipped:
+        assert innerpath is not None, "Must specify innerpath for zip files."
+    descriptor = {
         "name": make_resource_name(name),
         "type": "table",
         "path": path,
         "scheme": "file",
         "format": "tsv",
         "mediatype": "text/tsv",
+    }
+    if is_zipped:
+        descriptor["compression"] = "zip"
+        descriptor["innerpath"] = innerpath
+    descriptor.update({
         "encoding": "utf-8",
         "dialect": {
             "csv": {
@@ -102,7 +126,9 @@ def assemble_resource_descriptor(name, path, schema):
             }
         },
         "schema": schema
-    }
+    })
+    return descriptor
+
 
 def replace_extension(filepath: str, new_extension: str) -> str:
     if new_extension[0] != '.':
@@ -113,11 +139,61 @@ def make_json_path(file: File) -> str:
     return os.path.join(file.directory, f"{file.fname}.resource.json")
 
 SCHEMAS_DIR = os.path.join(os.path.dirname(__file__), 'schemas')
+os.makedirs(SCHEMAS_DIR, exist_ok=True)
 
-def get_schema_or_url(facet: str, column_names: Sequence[str],):
-    column_initials = ''.join(str(col)[0] for col in column_names)
-    schema_name = f"{facet}_{column_initials}"
-    schema_filename = f"{schema_name}.schema.json"
+# SCHEMA_REGISTRY_PATH = os.path.join(SCHEMAS_DIR, 'schema_registry.json')
+# SCHEMA_REGISTRY: dict = None
+#
+#
+# def get_schema_registry() -> dict:
+#     global SCHEMA_REGISTRY
+#     if SCHEMA_REGISTRY is None:
+#         SCHEMA_REGISTRY = dict()
+#         if os.path.exists(SCHEMA_REGISTRY_PATH):
+#             with open(SCHEMA_REGISTRY_PATH, 'r') as f:
+#                 SCHEMA_REGISTRY = json.load(f)
+#     return SCHEMA_REGISTRY
+
+# def register_new_schema_locally(descriptor: dict):
+#     registry = get_schema_registry()
+#     facet = descriptor['facet']
+#     identifier = descriptor['identifier']
+#     filename = descriptor['filename']
+#     if facet in registry:
+#         if identifier in registry[facet]:
+#             raise ValueError(f"Schema {facet}/{identifier} already registered!")
+#         registry[facet][identifier] = filename
+#     else:
+#         registry[facet] = {identifier: filename}
+#     with open(SCHEMA_REGISTRY_PATH, 'w') as f:
+#         json.dump(registry, f, indent=2)
+
+def get_truncated_hash(S: str | Iterable[str],
+                       hash_func = hashlib.sha1,
+                       length = 10) -> str:
+    """Computes the given hashfunction for the given string(s), and truncates the result."""
+    if isinstance(S, str):
+        S = [S]
+    hasher = hash_func()
+    for s in S:
+        hasher.update(s.encode('utf-8'))
+    return urlsafe_b64encode(hasher.digest()[:length]).decode('utf-8').rstrip('=')
+
+
+
+
+
+
+def get_schema_or_url(facet: str,
+                      column_names: Tuple[str],
+                      index_levels: Optional[Tuple[str]] = None,
+                      ):
+    if index_levels is None:
+        column_names = tuple(column_names)
+    else:
+        column_names = tuple(index_levels) + tuple(column_names)
+    schema_identifier = get_truncated_hash(column_names)
+    schema_filename = f"{facet}_{schema_identifier}.schema.json"
     schema_path = os.path.join(SCHEMAS_DIR, schema_filename)
     if os.path.exists(schema_path):
         schema_url = f"https://raw.githubusercontent.com/johentsch/ms3/main/src/ms3/utils/schemas/{schema_filename}"
@@ -126,10 +202,20 @@ def get_schema_or_url(facet: str, column_names: Sequence[str],):
             fl.Schema(schema_url)
             return schema_url
         except fl.FrictionlessException:
-            schema = fl.Schema(schema_path)
-            return schema.to_descriptor()
+            try:
+                schema = fl.Schema(schema_path)
+                return schema.to_descriptor()
+            except fl.FrictionlessException:
+                descriptor = json.load(open(schema_path, 'r'))
+                pprint(descriptor)
+                raise
     else:
-        descriptor = make_frictionless_descriptor(column_names=column_names)
+        descriptor = make_frictionless_schema_descriptor(column_names=column_names,
+                                                         primary_key=index_levels,
+                                                         facet=facet,
+                                                         identifier=schema_identifier,
+                                                         filename=schema_filename,
+                                                         )
         fl.Schema(descriptor).to_json(schema_path)
         return descriptor
 
